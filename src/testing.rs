ec4f66648788fb576f42aba0dc758e3225b1e441
Alex Johnson <alex_johnson3@yahoo.com>
2015-09-01 20:40:12 -0600

    Cleanup and refactoring of code from main and testing

---
+++ src/testing.rs
@@ -0,0 +1,47 @@
+#![cfg(test)]
+use test::Bencher;
+use types::*;
+use magics::*;
+use uci;
+
+static mut INITIALIZED: bool = false;
+
+pub fn check_init() {
+    unsafe {
+        if !INITIALIZED {
+            uci::init();
+            INITIALIZED = true;
+        }
+    }
+}
+
+#[bench]
+pub fn move_gen(b: &mut Bencher) {
+    check_init();
+    let board = Board::start_position();
+
+    b.iter(|| {
+        board.get_moves();
+    });
+}
+
+// #[bench]
+// pub fn get_moves(b: &mut Bencher) {
+//     check_init();
+//     let mut res = 0;
+//     let c = 0;
+//
+//     b.iter(|| black_box({
+//         res |= bishop_moves(0, c);
+//         res |= bishop_moves(0, c);
+//         res |= bishop_moves(0, c);
+//         res |= bishop_moves(0, c);
+//         res |= bishop_moves(0, c);
+//
+//         res |= rook_moves(0, c);
+//         res |= rook_moves(0, c);
+//         res |= rook_moves(0, c);
+//         res |= rook_moves(0, c);
+//         res |= rook_moves(0, c);
+//     }));
+// }
--- src/uci.rs
+++ src/uci.rs
@@ -1,33 +1,16 @@
-//! This is the Crabby chess engine
-#![feature(slice_patterns, convert, test, associated_consts)]
-extern crate test;
-extern crate time;
-extern crate rand;
-
-use std::io::{stdin, BufReader};
 use std::io::prelude::*;
+use std::io::{stdin, BufReader};
 use std::fs::File;
-
-pub mod board;
-pub mod evaluation;
-pub mod magics;
-pub mod search;
-pub mod table;
-pub mod types;
-pub mod util;
-pub mod bench;
+use time;
 
 use types::*;
+use magics;
+use table;
 use search::Searcher;
 
 const ENGINE_NAME: &'static str = "Crabby";
 
-pub fn main() {
-    unsafe {
-        magics::init();
-        table::init();
-    }
-
+pub fn main_loop() {
     let stdin = stdin();
     let mut searcher = Searcher::new_start();
 
@@ -44,8 +27,8 @@ pub fn main() {
                 "position"   => searcher.position(&mut params),
                 "go"         => searcher.go(&mut params),
                 "perft"      => perft(&searcher.root, &mut params),
-                "testperf"   => test_positions("testing/positions/performance", &mut searcher, &mut |s| s.id()),
-                "testmove"   => test_positions("testing/positions/perftsuite.epd", &mut searcher,
+                "testperf"   => positions("testing/positions/performance", &mut searcher, &mut |s| s.id()),
+                "testmove"   => positions("testing/positions/perftsuite.epd", &mut searcher,
                                                 &mut |s| println!("{}", s.root.perft(6, true))),
                 "print"      => (),
                 _            => println!("Unknown command: {}", first_word)
@@ -57,19 +40,13 @@ pub fn main() {
 pub fn perft(board: &Board, params: &mut Params) {
     let d = match params.next() {
         Some(val) => val.parse::<u8>().unwrap_or(1),
-        None       => 5
+        None      => 5
     };
 
     println!("total = {}\n", board.perft(d, true));
 }
 
-fn uci() {
-    println!("id name {}", ENGINE_NAME);
-    println!("id author Alex Johnson");
-    println!("uciok");
-}
-
-fn test_positions(path: &str, searcher: &mut Searcher, do_work: &mut FnMut(&mut Searcher)) {
+pub fn positions(path: &str, searcher: &mut Searcher, do_work: &mut FnMut(&mut Searcher)) {
     let file = match File::open(path) {
         Ok(file) => BufReader::new(file),
         Err(e)   => panic!("Test suite {} could not be read. {:?}", path, e)
@@ -85,4 +62,17 @@ fn test_positions(path: &str, searcher: &mut Searcher, do_work: &mut FnMut(&mut 
         do_work(searcher);
     }
     println!("Time taken = {} seconds", time::precise_time_s() - start);
+}
+
+pub fn init() {
+    unsafe {
+        magics::init();
+        table::init();
+    }
+}
+
+pub fn uci() {
+    println!("id name {}", ENGINE_NAME);
+    println!("id author Alex Johnson");
+    println!("uciok");
 }
